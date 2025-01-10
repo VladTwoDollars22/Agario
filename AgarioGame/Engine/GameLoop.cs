9ea3eb575a7de83a265b5282fe3564f4925c2167
VladTwoDollars22 <burkenavladislav@gmail.com>
2025-01-10 22:47:28 +0200

    creating a gameloop and gametime

---
+++ AgarioGame/Engine/GameLoop.cs
@@ -0,0 +1,78 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+using AgarioGame.Engine;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace AgarioGame
+{
+    public abstract class GameLoop
+    {
+        private Window window;
+
+        private int targetFPS;
+
+        private float _updateTrigger;
+        public GameLoop()
+        {
+            window = new RenderWindow(new VideoMode(1600, 900), "Game window");
+
+            targetFPS = 120;
+
+            _updateTrigger = 1f / targetFPS;
+        }
+        public void MainGameLoop()
+        {
+            Initialisation();
+
+            Clock clock = new Clock();
+            long lastFrameTime = clock.ElapsedTime.AsMilliseconds();
+
+            while (window.IsOpen)
+            {
+                long currentTime = clock.ElapsedTime.AsMilliseconds();
+                GameTime.SetDeltaTime((currentTime - lastFrameTime) / 1000f);
+
+                InputProcess();
+
+                if (GameTime.DeltaTime  > _updateTrigger)
+                {
+                    lastFrameTime = currentTime;
+
+                    Logic();
+                    Render();
+                }
+            }
+        }
+
+        private void Initialisation()
+        {
+            InitializeWindowEvents();
+        }
+        private void InitializeWindowEvents()
+        {
+            window.Closed += WindowClosed;
+        }
+        public abstract void GameInitialisation();
+
+        private void InputProcess()
+        {
+            window.DispatchEvents();
+        }
+
+        private void Logic()
+        {
+
+        }
+
+        private void Render()
+        {
+            window.Display();
+        }
+        static void WindowClosed(object sender, EventArgs e)
+        {
+            RenderWindow w = (RenderWindow)sender;
+            w.Close();
+        }
+    }
+}
---
+++ AgarioGame/Engine/GameTime.cs
@@ -0,0 +1,20 @@
+﻿
+namespace AgarioGame.Engine
+{
+    public static class GameTime
+    {
+        private static float deltaTime = 0;
+        private static float timeScale = 1f;
+
+        public static float DeltaTime => deltaTime * timeScale;
+
+        public static void SetDeltaTime(float currentDelta)
+        {
+            deltaTime = currentDelta;
+        }
+        public static void SetTimeScale(float newTimeScale)
+        {
+            timeScale = newTimeScale;
+        }
+    }
+}
