453dd82c43a9a93ccae383cec8c4bedc6ca5f009
VladTwoDollars22 <burkenavladislav@gmail.com>
2025-02-18 17:58:34 +0200

    Splitting the timer from the timer manager

--- AgarioGame/Engine/Core/Time/TimerManager.cs
+++ AgarioGame/Engine/Core/Time/TimerManager.cs
@@ -3,45 +3,6 @@
     public class TimerManager : IUpdatable
     {
         public static TimerManager Instance = new();
-        private class Timer
-        {
-            public Action Action;
-            public float RemainingTime;
-            public bool IsRepeating;
-            public float Interval;
-            public int RepeatCount;
-
-            public Timer(Action action, float time, bool repeating, int repeatCount = -1)
-            {
-                Action = action;
-                RemainingTime = time;
-                IsRepeating = repeating;
-                Interval = time;
-                RepeatCount = repeatCount;
-            }
-
-            public bool Tick(float deltaTime)
-            {
-                RemainingTime -= deltaTime;
-                if (RemainingTime > 0) return false; 
-
-                Action.Invoke();
-
-                if (IsRepeating)
-                {
-                    if (RepeatCount > 0)
-                    {
-                        RepeatCount--;
-                        if (RepeatCount == 0) return true;
-                    }
-                    RemainingTime = Interval;
-                    return false;
-                }
-
-                return true;
-            }
-        }
-
         private List<Timer> timers = new List<Timer>();
         private Dictionary<string, Timer> namedIntervals = new Dictionary<string, Timer>();
 
