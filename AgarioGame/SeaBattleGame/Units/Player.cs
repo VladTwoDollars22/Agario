08a140290dcc5663b296232cf5992951acfecca5
VladTwoDollars22 <burkenavladislav@gmail.com>
2025-02-27 20:11:40 +0200

    Writing a player

--- AgarioGame/SeaBattleGame/Units/Player.cs
+++ AgarioGame/SeaBattleGame/Units/Player.cs
@@ -1,12 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AgarioGame.SeaBattleGame.Units
+﻿namespace AgarioGame.SeaBattleGame.Units
 {
-    internal class Player
+    public class Player
     {
+        public string NickName;
+        public List<int> ships;
+
+        public int HP { get; private set; }
+
+        public (int width, int heigth) radarArea;
+        public bool usingRadar = false;
+        public (int x, int y) radarPoint;
+        public int radarsCount;
+
+        public (int width, int height) fieldSize;
+
+        public Player(string nickName)
+        {
+            NickName = nickName;
+            radarsCount = 1;
+            radarPoint = (-1, -1);
+            radarArea = (3, 3);
+
+            fieldSize = (9, 9);
+
+            ships = new List<int> { 4, 3, 3, 2, 2, 2, 1, 1, 1, 1 };
+
+            HP = ships.Sum();
+        }
+
+        public void TakeDamage(int damage)
+        {
+            HP -= damage;
+        }
+
+        public void UseRadar()
+        {
+            radarsCount--;
+        }
+        public void Reset()
+        {
+            HP = ships.Sum();
+        }
     }
 }
